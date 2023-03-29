c7063009380e441e090aca379211c2367e735525
Roman Zelenin (ZelRS) <ZeleninRS1991@gmail.com>
2023-03-29 20:46:27 +0600

    Сделал домашнее задание 5

--- src/Main.java
+++ src/Main.java
@@ -6,24 +6,100 @@ public class Main {
         task4();
         task5();
     }
-
     public static void task1() {
         System.out.println("Задача 1");
+        byte clientOS = 0;
+        switch (clientOS) {
+            case 0:
+                System.out.println("Установите версию приложения для iOS по ссылке.");
+                break;
+            case 1:
+                System.out.println("Установите версию приложения для Android по ссылке");
+                break;
+            default:
+                System.out.println("Неизвестное число");
+        }
     }
-
     public static void task2() {
         System.out.println("Задача 2");
+        byte clientOS = 1;
+        short clientDeviceYear = 2014;
+        switch (clientOS) {
+            case 0:
+                if (clientDeviceYear >= 2015) {
+                    System.out.println("Установите версию приложения для iOS по ссылке.");
+                }
+                else {
+                    System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+                    }
+                break;
+            case 1:
+                if (clientDeviceYear >= 2015) {
+                    System.out.println("Установите версию приложения для Android по ссылке.");
+                }
+                else {
+                    System.out.println("Установите облегченную версию приложения для Android по ссылке");
+                }
+                break;
+            default:
+                System.out.println("Неизвестное число");
+        }
     }
-
     public static void task3() {
         System.out.println("Задача 3");
+        short year = 2200;
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + " год является високосным");
+        }
+        else {
+            System.out.println(year + " год НЕ является високосным");
+        }
     }
-
     public static void task4() {
         System.out.println("Задача 4");
+        int deliveryDistance = 65;
+        byte deliveryDays;
+        if (deliveryDistance >= 100) {
+            System.out.println("Доставки нет");
+        }
+        else {
+            deliveryDays = 1;
+            if (deliveryDistance > 20 && deliveryDistance <= 100) {
+                deliveryDays++;
+            }
+            if (deliveryDistance > 60 && deliveryDistance <= 100) {
+                deliveryDays++;
+            }
+            System.out.println("Требуется дней: " + deliveryDays);
+        }
     }
-
     public static void task5() {
         System.out.println("Задача 5");
+        byte monthNumber = 6;
+        switch (monthNumber) {
+            case 1:
+            case 2:
+            case 12:
+                System.out.println("Это зимний месяц");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Это весенний месяц");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Это летний месяц");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Это осенний месяц");
+                break;
+            default:
+                System.out.println("Неизвестное число");
+
+        }
     }
 }
